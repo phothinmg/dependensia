2295f22b02ba4a70c79a0435f19285c887ec0387
phothinmg <phothinmg@disroot.org>
2025-09-12 03:28:51 +0630

    ptm202581232827

--- opt/push.ts
+++ opt/push.ts
@@ -5,35 +5,62 @@ const dt = new Date();
 const msg = `ptm${dt.getFullYear()}${dt.getMonth()}${dt.getDate()}${dt.getHours()}${dt.getMinutes()}${dt.getSeconds()}`;
 
 const pcJobs = [
-  async () => {
-    await wait(1000);
-    await new Promise<void>((resolve, reject) => {
-      const cp = exec("git add .");
-      cp.stdout?.pipe(process.stdout);
-      cp.stderr?.pipe(process.stderr);
-      cp.once("exit", (code) => (code === 0 ? resolve() : reject()));
-    });
-  },
-  async () => {
-    await wait(1000);
-    await new Promise<void>((resolve, reject) => {
-      const cp = exec(`git commit -S -m "${msg}"`);
-      cp.stdout?.pipe(process.stdout);
-      cp.stderr?.pipe(process.stderr);
-      cp.once("exit", (code) => (code === 0 ? resolve() : reject()));
-    });
-  },
-  async () => {
-    await wait(1000);
-    await new Promise<void>((resolve, reject) => {
-      const cp = exec("git push");
-      cp.stdout?.pipe(process.stdout);
-      cp.stderr?.pipe(process.stderr);
-      cp.once("exit", (code) => (code === 0 ? resolve() : reject()));
-    });
-  },
+	async () => {
+		await wait(1000);
+		await new Promise<void>((resolve, reject) => {
+			const cp = exec("npm run lint");
+			cp.stdout?.pipe(process.stdout);
+			cp.stderr?.pipe(process.stderr);
+			cp.once("exit", (code) => (code === 0 ? resolve() : reject()));
+		});
+	},
+	async () => {
+		await wait(1000);
+		await new Promise<void>((resolve, reject) => {
+			const cp = exec("npm run format");
+			cp.stdout?.pipe(process.stdout);
+			cp.stderr?.pipe(process.stderr);
+			cp.once("exit", (code) => (code === 0 ? resolve() : reject()));
+		});
+	},
+	async () => {
+		await wait(1000);
+		await new Promise<void>((resolve, reject) => {
+			const cp = exec("npm run coverage");
+			cp.stdout?.pipe(process.stdout);
+			cp.stderr?.pipe(process.stderr);
+			cp.once("exit", (code) => (code === 0 ? resolve() : reject()));
+		});
+	},
+	async () => {
+		await wait(1000);
+		await new Promise<void>((resolve, reject) => {
+			const cp = exec("git add .");
+			cp.stdout?.pipe(process.stdout);
+			cp.stderr?.pipe(process.stderr);
+			cp.once("exit", (code) => (code === 0 ? resolve() : reject()));
+		});
+	},
+	async () => {
+		await wait(1000);
+		await new Promise<void>((resolve, reject) => {
+			const cp = exec(`git commit -S -m "${msg}"`);
+			cp.stdout?.pipe(process.stdout);
+			cp.stderr?.pipe(process.stderr);
+			cp.once("exit", (code) => (code === 0 ? resolve() : reject()));
+		});
+	},
+	async () => {
+		await wait(1000);
+		await new Promise<void>((resolve, reject) => {
+			const cp = exec("git push");
+			cp.stdout?.pipe(process.stdout);
+			cp.stderr?.pipe(process.stderr);
+			cp.once("exit", (code) => (code === 0 ? resolve() : reject()));
+		});
+	},
 ];
 
 for (const job of pcJobs) {
-  await job();
+	await job();
 }

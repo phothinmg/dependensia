f9a695fe27049f7fab172f6212597ef0ac0ba9ab
phothinmg <phothinmg@disroot.org>
2025-09-12 03:17:09 +0630

    ptm20258123177

--- opt/push.ts
+++ opt/push.ts
@@ -8,6 +8,15 @@ const pcJobs = [
   async () => {
     await wait(1000);
     await new Promise<void>((resolve, reject) => {
+      const cp = exec("git add .");
+      cp.stdout?.pipe(process.stdout);
+      cp.stderr?.pipe(process.stderr);
+      cp.once("exit", (code) => (code === 0 ? resolve() : reject()));
+    });
+  },
+  async () => {
+    await wait(1000);
+    await new Promise<void>((resolve, reject) => {
       const cp = exec(`git commit -S -m "${msg}"`);
       cp.stdout?.pipe(process.stdout);
       cp.stderr?.pipe(process.stderr);

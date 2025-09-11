280fdbe7f21b5d1b58fc94edee03585f36f9a817
phothinmg <phothinmg@disroot.org>
2025-09-12 03:00:09 +0630

    ptm2025812307

--- opt/push.ts
+++ opt/push.ts
@@ -11,50 +11,50 @@ const dt = new Date();
 const msg = `ptm${dt.getFullYear()}${dt.getMonth()}${dt.getDate()}${dt.getHours()}${dt.getMinutes()}${dt.getSeconds()}`;
 
 const pcJobs = [
-	async () => {
-		if (fs.existsSync(hookLinkPC)) {
-			await fs.promises.unlink(hookLinkPC);
-		}
-	},
-	async () => {
-		await wait(1000);
-		await fs.promises.copyFile(
-			optLinkPC,
-			hookLinkPC,
-			fs.promises.constants.COPYFILE_EXCL,
-		);
-	},
-	async () => {
-		if (fs.existsSync(hookLinkPP)) {
-			await fs.promises.unlink(hookLinkPP);
-		}
-	},
-	async () => {
-		await wait(1000);
-		await fs.promises.copyFile(
-			optLinkPP,
-			hookLinkPP,
-			fs.promises.constants.COPYFILE_EXCL,
-		);
-	},
-	async () => {
-		await wait(1000);
-		await new Promise<void>((resolve, reject) => {
-			const cp = exec("chmod +x .git/hooks/pre-commit");
-			cp.stdout?.pipe(process.stdout);
-			cp.stderr?.pipe(process.stderr);
-			cp.once("exit", (code) => (code === 0 ? resolve() : reject()));
-		});
-	},
-	async () => {
-		await wait(1000);
-		await new Promise<void>((resolve, reject) => {
-			const cp = exec("chmod +x .git/hooks/pre-push");
-			cp.stdout?.pipe(process.stdout);
-			cp.stderr?.pipe(process.stderr);
-			cp.once("exit", (code) => (code === 0 ? resolve() : reject()));
-		});
-	},
+	// async () => {
+	// 	if (fs.existsSync(hookLinkPC)) {
+	// 		await fs.promises.unlink(hookLinkPC);
+	// 	}
+	// },
+	// async () => {
+	// 	await wait(1000);
+	// 	await fs.promises.copyFile(
+	// 		optLinkPC,
+	// 		hookLinkPC,
+	// 		fs.promises.constants.COPYFILE_EXCL,
+	// 	);
+	// },
+	// async () => {
+	// 	if (fs.existsSync(hookLinkPP)) {
+	// 		await fs.promises.unlink(hookLinkPP);
+	// 	}
+	// },
+	// async () => {
+	// 	await wait(1000);
+	// 	await fs.promises.copyFile(
+	// 		optLinkPP,
+	// 		hookLinkPP,
+	// 		fs.promises.constants.COPYFILE_EXCL,
+	// 	);
+	// },
+	// async () => {
+	// 	await wait(1000);
+	// 	await new Promise<void>((resolve, reject) => {
+	// 		const cp = exec("chmod +x .git/hooks/pre-commit");
+	// 		cp.stdout?.pipe(process.stdout);
+	// 		cp.stderr?.pipe(process.stderr);
+	// 		cp.once("exit", (code) => (code === 0 ? resolve() : reject()));
+	// 	});
+	// },
+	// async () => {
+	// 	await wait(1000);
+	// 	await new Promise<void>((resolve, reject) => {
+	// 		const cp = exec("chmod +x .git/hooks/pre-push");
+	// 		cp.stdout?.pipe(process.stdout);
+	// 		cp.stderr?.pipe(process.stderr);
+	// 		cp.once("exit", (code) => (code === 0 ? resolve() : reject()));
+	// 	});
+	// },
 	async () => {
 		await wait(1000);
 		await new Promise<void>((resolve, reject) => {
